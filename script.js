3d6e2a1adabf3bef07a8919b824bdcec45be7c61
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-04-14 19:45:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let firstName=document.ElementById("fname").value;
-let lastName=document.ElementById("lname").value;
+
 function getFormvalue(event) {
     //Write your code here
 event.preventDefault();
-	
+	let firstName=document.ElementById("fname").value;
+let lastName=document.ElementById("lname").value;
+	alert("firstName");
 }
+document.getElementById("btn").addEventListener("click", getFormvalue);
