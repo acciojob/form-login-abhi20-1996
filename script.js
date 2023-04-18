e6a459e23a02a96d6e45d6e36db5d82407334c56
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-04-18 11:45:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 function getFormvalue(event) {
     //Write your code here
-event.preventDefault();
+//event.preventDefault();
 	let firstName=document.getElementsByName("fname");
 	let lastName=document.getElementsByName("lname");
-	alert(firstName);
+	alert(firstName[0].value);
 }
 
 //document.getElementById("btn").addEventListener("click", getFormvalue);
